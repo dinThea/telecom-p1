1987ebea445ed657ee8059b14f5a785f82d6e263
Iago Barbosa <iago@visio.ai>
2023-07-24 23:33:59 -0300

    feat(uart): trivial uart test solved

--- uart.cpp
+++ uart.cpp
@@ -1,8 +1,71 @@
 #include "uart.hpp"
+#include <iostream>
+#include <bitset>
 
 void UART_RX::put_samples(const unsigned int *buffer, unsigned int n)
 {
-    // seu código aqui
+
+    for (int i = 0; i < n; i++) {
+        if (state == State::Waiting) {
+            if (buffer[i] == 1) {
+                continue;
+            } else {
+                // std::cout << std::endl;
+                // std::cout << "Receiving Start" << std::endl;
+                // std::cout << buffer[i];
+                state = State::ReceivingStart;
+                sample_count++;
+            }
+        } else if (state == State::ReceivingStart) {
+            if (sample_count < SAMPLES_PER_SYMBOL) {
+                // std::cout << buffer[i];
+                sample_count++;
+            } else {
+                // std::cout << std::endl;
+                // std::cout << "Receiving" << std::endl;
+                // std::cout << buffer[i];
+                sample_count=0;
+                state = State::Receiving;
+            }
+        } else if (state == State::Receiving) {
+            if (bits_count < 7) {
+                if (sample_count < SAMPLES_PER_SYMBOL-1) {
+                    sample_count++;
+                    // std::cout << buffer[i];
+                } else {
+                    // std::cout << std::endl;
+                    sample_count = 0;
+                    // std::cout << "BIT: "<< buffer[i-1] << std::endl;
+                    // std::cout << buffer[i];
+                    byte |= (buffer[i-1] << bits_count);
+                    // byte <<= 1;
+                    bits_count++;
+                }
+            } else {
+                byte |= (buffer[i-1] << bits_count);
+                byte_counter ++;
+                std::bitset<8> bit_byte(byte);
+                // std::cout << "BYTE NO " << byte_counter << " " << bit_byte << std::endl;
+                get_byte(byte);
+                byte = 0;
+                // std::cout << "Receiving End" << std::endl;
+                // std::cout << buffer[i];
+                sample_count=2;
+                bits_count=0;
+                state = State::ReceivingEnd;
+            }
+        } else if (state == State::ReceivingEnd) {
+            if (sample_count < SAMPLES_PER_SYMBOL - 1) {
+                sample_count++;
+                // std::cout << buffer[i];
+            } else {
+                // std::cout << buffer[i] << std::endl;
+                // std::cout << "Waiting" << std::endl;
+                sample_count=0;
+                state = State::Waiting;
+            }
+        }
+    }
 }
 
 void UART_TX::put_byte(uint8_t byte)
--- uart.hpp
+++ uart.hpp
@@ -7,13 +7,26 @@
 #include <stdint.h>
 #include "config.hpp"
 
+enum class State { Waiting, ReceivingStart, Receiving, ReceivingEnd }; 
 class UART_RX
 {
 public:
-    UART_RX(std::function<void(uint8_t)> get_byte) :get_byte(get_byte) {}
+    UART_RX(std::function<void(uint8_t)> get_byte) :get_byte(get_byte) {
+        bits_count = 0;
+        sample_count = 0;
+        byte = 0;
+        byte_counter = 0;
+        State state = State::Waiting;
+    }
     void put_samples(const unsigned int *buffer, unsigned int n);
 private:
     std::function<void(uint8_t)> get_byte;
+    unsigned int bits_count;
+    unsigned int sample_count;
+    uint8_t byte;
+    unsigned int byte_counter;
+    State state;
+
 };
 
 class UART_TX
